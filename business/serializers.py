72ab0a1b57661dfae9d42bbc44f7fb388b91bc4a
FrancisLeigh <git@francisleigh.co.uk>
2018-09-12 17:13:17 +0100

    update to shops serializer

--- business/serializers.py
+++ business/serializers.py
@@ -1,21 +1,23 @@
 from rest_framework import serializers
 from .models import Business, Shop, Chair
 
+import clr
+
 class BusinessSerializer(serializers.ModelSerializer):
   class Meta:
     model = Business
 
     fields = '__all__'
 
-class ShopsSerializer(serializers.ModelSerializer):
-  # chairs = serializers.PrimaryKeyRelatedField(read_only=True)
-  class Meta:
-    model = Shop
-
-    fields = '__all__'
-
 class ChairsSerializer(serializers.ModelSerializer):
   class Meta:
     model = Chair
+
+    fields = '__all__'
+
+class ShopsSerializer(serializers.ModelSerializer):
+  chairs = ChairsSerializer(many=True)
+  class Meta:
+    model = Shop
 
     fields = '__all__'
